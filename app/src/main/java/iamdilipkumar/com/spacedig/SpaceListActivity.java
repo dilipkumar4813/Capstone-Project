c46203c3b87851544fe0655f0ce4d25f482dab63
Dilip <dilipkumar4813@gmail.com>
2017-06-18 19:24:17 +0530

    Initial commit

---
+++ app/src/main/java/iamdilipkumar/com/spacedig/SpaceListActivity.java
@@ -0,0 +1,13 @@
+package iamdilipkumar.com.spacedig;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpaceListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_list);
+    }
+}
