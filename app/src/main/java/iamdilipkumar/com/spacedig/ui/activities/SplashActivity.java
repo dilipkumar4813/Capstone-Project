6d5f8e2afdcfada738e9a9a95fe58e6dc92c171a
Dilip <dilipkumar4813@gmail.com>
2017-06-27 22:33:54 +0530

    Splash activity for the application

---
+++ app/src/main/java/iamdilipkumar/com/spacedig/ui/activities/SplashActivity.java
@@ -0,0 +1,20 @@
+package iamdilipkumar.com.spacedig.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created on 27/06/17.
+ *
+ * @author dilipkumar4813
+ * @version 1.0
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
