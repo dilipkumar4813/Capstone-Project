b4629ac57c8f09c308c7bcacc9c4966f2d4d4931
Dilip <dilipkumar4813@gmail.com>
2017-06-28 23:30:32 +0530

    Adding splash activity as the loading activity

--- app/src/main/java/iamdilipkumar/com/spacedig/ui/activities/SplashActivity.java
+++ app/src/main/java/iamdilipkumar/com/spacedig/ui/activities/SplashActivity.java
@@ -1,6 +1,8 @@
 package iamdilipkumar.com.spacedig.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -16,5 +18,15 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent mainList = new Intent(this, SpaceListActivity.class);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashActivity.this.finish();
+                startActivity(mainList);
+            }
+        }, 1000);
     }
 }
