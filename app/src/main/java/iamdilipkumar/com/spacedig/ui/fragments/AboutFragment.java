80afa4724689317d49cf7d1aba44b7528c5d45e8
Dilip <dilipkumar4813@gmail.com>
2017-07-07 12:42:57 +0530

    APOD color change to match other parts of the application About us section to include privacy policy Refactoring of content apod

--- app/src/main/java/iamdilipkumar/com/spacedig/ui/fragments/AboutFragment.java
+++ app/src/main/java/iamdilipkumar/com/spacedig/ui/fragments/AboutFragment.java
@@ -1,12 +1,16 @@
 package iamdilipkumar.com.spacedig.ui.fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import iamdilipkumar.com.spacedig.R;
 
 
@@ -18,10 +22,21 @@ import iamdilipkumar.com.spacedig.R;
  */
 public class AboutFragment extends Fragment {
 
+    @OnClick(R.id.tv_privacy_policy)
+    void viewPrivacyPolicy() {
+        String url = "http://iamdilipkumar.com/index.php/space-dig-privacy-policy/";
+
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        ButterKnife.bind(this, view);
+
+        return view;
     }
 
 }
